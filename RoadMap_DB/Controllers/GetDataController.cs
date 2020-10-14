d4705b45245bc9fa3104c4db9dbf0ff6c2df2a37
Weerasingha <41960173+Erandamweerasinghe@users.noreply.github.com>
2020-10-15 01:13:30 +0530

    Add files via upload

---
+++ RoadMap_DB/Controllers/GetDataController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+using RoadMap_DB.Data;
+using RoadMap_DB.DataAccess;
+using RoadMap_DB.Models;
+
+namespace RoadMap_DB.Controllers
+{
+    public class GetDataController : Controller
+    {
+       
+        [Route("GetData/Index")]
+        public JsonResult Index()
+        {
+           
+           // string constr = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            // using (MySqlConnection con=new MySqlConnection(MySqlDataAccess.GetConnectionString("DefaultConnection")))
+
+            string sql = "SELECT  * FROM USERS ";
+            JsonResult result=MySqlDataAccess.AccesData(sql, new User());
+
+            Console.Write(result);
+            return new JsonResult(result);
+        }
+
+        [Route("GetData/DepartmentList")]
+        public JsonResult DepartmentList()
+        {
+
+            string sql = "SELECT  * FROM department_lists ";
+            JsonResult result = MySqlDataAccess.AccesData(sql, new Department_list());
+
+            Console.Write(result);
+            return new JsonResult(result);
+
+        }
+
+        [Route("GetData/VRoute")]
+        public JsonResult VRoute()
+        {
+
+            string sql = "SELECT  * FROM v_routes ";
+            JsonResult result = MySqlDataAccess.AccesData(sql, new v_route());
+
+            Console.Write(result);
+            return new JsonResult(result);
+
+        }
+
+        [Route("GetData/FRoute")]
+        public JsonResult FRoute()
+        {
+
+            string sql = "SELECT  * FROM v_routes ";
+            JsonResult result = MySqlDataAccess.AccesData(sql, new v_route());
+
+            Console.Write(result);
+            return new JsonResult(result);
+
+        }
+
+
+        [Route("GetData/UserLocation")]
+        public JsonResult UserLocation()
+        {
+
+            string sql = "SELECT  * FROM user_location ";
+            JsonResult result = MySqlDataAccess.AccesData(sql, new User_location());
+
+            Console.Write(result);
+            return new JsonResult(result);
+
+        }
+
+        [Route("GetData/Floor")]
+        public JsonResult Floor()
+        {
+
+            string sql = "SELECT  * FROM floors ";
+            JsonResult result = MySqlDataAccess.AccesData(sql, new Floor());
+
+            Console.Write(result);
+            return new JsonResult(result);
+
+        }
+    }
+}
---
+++ RoadMap_DB/Controllers/UserController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RoadMap_DB.Data;
+using RoadMap_DB.Models;
+
+namespace RoadMap_DB.Controllers
+{
+    public class UserController : Controller
+    {
+
+        private readonly ApplicationDBContext _db;
+
+        public UserController(ApplicationDBContext db)
+        {
+            this._db = db;
+        }
+
+
+        [Route("User/Index")]
+        public void  Index(string name,string email,string type,string password)
+        {
+            //insert User Data
+            User u = new User()
+            {
+                name=name,
+                email=email,
+                type=type,
+                pwd=password
+            };
+
+            _db.users.Add(u);
+            _db.SaveChanges();
+            //return View();
+        }
+    }
+}
