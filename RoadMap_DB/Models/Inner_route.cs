41b315784223eb194fce461d74cc13f370567794
Weerasingha <41960173+Erandamweerasinghe@users.noreply.github.com>
2020-10-21 03:06:26 +0530

    Add files via upload

--- RoadMap_DB/Models/Inner_route.cs
+++ RoadMap_DB/Models/Inner_route.cs
@@ -1,19 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace RoadMap_DB.Models
 {
-   
-    public class f_route
-    {     
-        [Key,Column(Order =0)]
+    public class Inner_route
+    {
+        [Key, Column(Order = 0)]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int id { get; set; }
-        public int f_route_id { get; set; }
+        public int in_r_id { get; set; }
 
         [Column(Order = 2)]
         public double lat { get; set; }
--- RoadMap_DB/Models/Vertext_Location.cs
+++ RoadMap_DB/Models/Vertext_Location.cs
@@ -1,24 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace RoadMap_DB.Models
 {
-   
-    public class f_route
-    {     
+    public class Vertext_Location
+    {
         [Key,Column(Order =0)]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int id { get; set; }
-        public int f_route_id { get; set; }
-
-        [Column(Order = 2)]
         public double lat { get; set; }
-
-        [Column(Order = 3)]
         public double lng { get; set; }
     }
 }
