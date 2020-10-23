52180a4f4a28c5c07db82972f5fdeadeb0b48269
Dilan <dilanchanukalasantha@gmail.com>
2020-10-23 11:35:49 +0530

    Request handling methds modified

--- Road-Map-Web-API/Road-Map-Web-API/Data.cs
+++ Road-Map-Web-API/Road-Map-Web-API/Data.cs
@@ -11,9 +11,9 @@ namespace Road_Map_Web_API
 
         //no of vertices in the graphs
         public static int footGrapheVertices = 9;
-        public static int innerGrapheVertices_1 = 9;
-        public static int innerGrapheVertices_2 = 9;
-        public static int innerGrapheVertices_3 = 9;
+        public static int innerGraphe_0_Vertices = 9;
+        public static int innerGraphe_1_Vertices = 9;
+        public static int innerGraphe_2_Vertices = 9;
         public static int vehicleGrapheVertices = 9;
 
         //end points of each route
@@ -21,15 +21,15 @@ namespace Road_Map_Web_API
         {
             {0,1 },{1,2 },{2,3 },{3,4 },{ 4,5},{5,6 },{6,7 },{0,7 },{1,7 },{ 7,8},{ 6,8},{2,8 },{2,5 },{ 3,5}
         };
-        public static int[,] innerRouteEndpoints_1 = new int[,]
+        public static int[,] inner_0_RouteEndpoints = new int[,]
         {
             {0,1 },{1,2 },{2,3 },{3,4 },{ 4,5},{5,6 },{6,7 },{0,7 },{1,7 },{ 7,8},{ 6,8},{2,8 },{2,5 },{ 3,5}
         };
-        public static int[,] innerRouteEndpoints_2 = new int[,]
+        public static int[,] inner_1_RouteEndpoints = new int[,]
         {
             {0,1 },{1,2 },{2,3 },{3,4 },{ 4,5},{5,6 },{6,7 },{0,7 },{1,7 },{ 7,8},{ 6,8},{2,8 },{2,5 },{ 3,5}
         };
-        public static int[,] innerRouteEndpoints_3 = new int[,]
+        public static int[,] inner_2_RouteEndpoints = new int[,]
         {
             {0,1 },{1,2 },{2,3 },{3,4 },{ 4,5},{5,6 },{6,7 },{0,7 },{1,7 },{ 7,8},{ 6,8},{2,8 },{2,5 },{ 3,5}
         };
@@ -40,6 +40,16 @@ namespace Road_Map_Web_API
 
         //graphs of each map
         public static int[,] flootRoutesGraph = new int[,] { { 0, 4, 0, 0, 0, 0, 0, 8, 0 },
+                                                      { 4, 0, 8, 0, 0, 0, 0, 11, 0 },
+                                                      { 0, 8, 0, 7, 0, 4, 0, 0, 2 },
+                                                      { 0, 0, 7, 0, 9, 14, 0, 0, 0 },
+                                                      { 0, 0, 0, 9, 0, 10, 0, 0, 0 },
+                                                      { 0, 0, 4, 14, 10, 0, 2, 0, 0 },
+                                                      { 0, 0, 0, 0, 0, 2, 0, 1, 6 },
+                                                      { 8, 11, 0, 0, 0, 0, 1, 0, 7 },
+                                                      { 0, 0, 2, 0, 0, 0, 6, 7, 0 } };
+
+        public static int[,] innerRoutesGraph_0 = new int[,] { { 0, 4, 0, 0, 0, 0, 0, 8, 0 },
                                                       { 4, 0, 8, 0, 0, 0, 0, 11, 0 },
                                                       { 0, 8, 0, 7, 0, 4, 0, 0, 2 },
                                                       { 0, 0, 7, 0, 9, 14, 0, 0, 0 },
@@ -69,16 +79,6 @@ namespace Road_Map_Web_API
                                                       { 8, 11, 0, 0, 0, 0, 1, 0, 7 },
                                                       { 0, 0, 2, 0, 0, 0, 6, 7, 0 } };
 
-        public static int[,] innerRoutesGraph_3 = new int[,] { { 0, 4, 0, 0, 0, 0, 0, 8, 0 },
-                                                      { 4, 0, 8, 0, 0, 0, 0, 11, 0 },
-                                                      { 0, 8, 0, 7, 0, 4, 0, 0, 2 },
-                                                      { 0, 0, 7, 0, 9, 14, 0, 0, 0 },
-                                                      { 0, 0, 0, 9, 0, 10, 0, 0, 0 },
-                                                      { 0, 0, 4, 14, 10, 0, 2, 0, 0 },
-                                                      { 0, 0, 0, 0, 0, 2, 0, 1, 6 },
-                                                      { 8, 11, 0, 0, 0, 0, 1, 0, 7 },
-                                                      { 0, 0, 2, 0, 0, 0, 6, 7, 0 } };
-
         public static int[,] vehicleRoutesGraph = new int[,] { { 0, 4, 0, 0, 0, 0, 0, 8, 0 },
                                                       { 4, 0, 8, 0, 0, 0, 0, 11, 0 },
                                                       { 0, 8, 0, 7, 0, 4, 0, 0, 2 },
@@ -89,15 +89,20 @@ namespace Road_Map_Web_API
                                                       { 8, 11, 0, 0, 0, 0, 1, 0, 7 },
                                                       { 0, 0, 2, 0, 0, 0, 6, 7, 0 } };
 
-        public static int[] InnerGraph_1_Places = new int[] { 4, 5, 6, 7, 8, 9 };
-        public static int[] InnerGraph_2_Places = new int[] { 4, 5, 6, 7, 8, 9 };
-        public static int[] InnerGraph_3_Places = new int[] { 4, 5, 6, 7, 8, 9 };
+        //includes place numbers for relavelt inner graph vertex number 
+        //index=inner vertex number,value=place id
+        public static int[] InnerPlaceMatch_0 = new int[] { 4, 5, 6, 7, 8, 9 };
+        public static int[] InnerPlaceMatch_1 = new int[] { 4, 5, 6, 7, 8, 9 };
+        public static int[] InnerPlaceMatch_2 = new int[] { 4, 5, 6, 7, 8, 9 };
 
+        //includes foot graph vertex numbers for relavelt inner graph 
+        //index=inner vetex number,value=foot graph vertex no
+        public static int[] InnerOuterMatch_0 = new int[] { 4, 5, 6, 7, 8, 9 };
         public static int[] InnerOuterMatch_1 = new int[] { 4, 5, 6, 7, 8, 9 };
         public static int[] InnerOuterMatch_2 = new int[] { 4, 5, 6, 7, 8, 9 };
-        public static int[] InnerOuterMatch_3 = new int[] { 4, 5, 6, 7, 8, 9 };
 
         //includes foot graph vertex numbers for relavelt entrance number
+        //index=entrance no,value=foot graph vertex no
         public static int[] EntranceOuterMatch = new int[] { 4, 5, 6, 7};
 
 
