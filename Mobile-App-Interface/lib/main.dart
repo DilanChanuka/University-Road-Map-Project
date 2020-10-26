de4536e210fe3cf2e5a85cfe3dd1a77a3902abeb
Weerasingha <41960173+Erandamweerasinghe@users.noreply.github.com>
2020-10-27 00:43:13 +0530

    Add files via upload

---
+++ Mobile-App-Interface/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:uor_road_map/Screens/Welcome/welcome_page.dart';
+//import 'package:uor_road_map/Screens/Map/map_show_page.dart';
+//import 'package:uor_road_map/Screens/Search/search_page.dart';
+//import 'package:uor_road_map/Screens/AddSearch/add_search_page.dart';
+import 'package:uor_road_map/Screens/Term&Con/term_con_page.dart';
+import 'package:uor_road_map/Screens/SignUp/signup_page.dart';
+import 'package:uor_road_map/Screens/FPassword/forg_pass_page.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.black26,
+      ),
+      home:WelcomePage(), 
+    );
+  }
+}
+
