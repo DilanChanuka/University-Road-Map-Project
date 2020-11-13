4cefbc74072f6e2208dc83b6823814abdfe9b3b7
Weerasingha <41960173+Erandamweerasinghe@users.noreply.github.com>
2020-11-13 13:32:52 +0530

    Update JsonBody.dart

--- Mobile-App-Interface/lib/Screens/Request/JsonBody.dart
+++ Mobile-App-Interface/lib/Screens/Request/JsonBody.dart
@@ -11,6 +11,6 @@ import 'package:uor_road_map/Screens/Map/drwaplace.dart';
          finaldata=data.body;        
       }
     
-     String a='{"floor_2_locations":[[7.357911, 80.935412],[7.352394, 80.930181],[7.348597, 80.935259],[7.353203, 80.942557]],"places":[{"name":"Instructore\u0027s Room","lat":7.353330,"lon":80.936152},{"name":"Computer Lab 1","lat":7.353178,"lon": 80.934136},{"name":"Lecturers\u0027s Room","lat":7.354697,"lon": 80.934697}],"place":{"name":"Mini Auditorium","lat":7.353229,"lon":80.933804}}';
-      return a;
+     //String a='{"floor_2_locations":[[7.357911, 80.935412],[7.352394, 80.930181],[7.348597, 80.935259],[7.353203, 80.942557]],"places":[{"name":"Instructore\u0027s Room","lat":7.353330,"lon":80.936152},{"name":"Computer Lab 1","lat":7.353178,"lon": 80.934136},{"name":"Lecturers\u0027s Room","lat":7.354697,"lon": 80.934697}],"place":{"name":"Mini Auditorium","lat":7.353229,"lon":80.933804}}';
+      return finaldata;
   }
