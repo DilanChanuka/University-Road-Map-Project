23cc639b1ccfe97bb86dc2c049a8a7478c2e8825
Weerasingha <41960173+Erandamweerasinghe@users.noreply.github.com>
2020-11-24 23:40:48 +0530

    Add files via upload

--- Mobile-App-Interface/ios/Flutter/flutter_export_environment.sh
+++ Mobile-App-Interface/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=E:\Softwares\Android\Flutter\flutter"
-export "FLUTTER_APPLICATION_PATH=F:\Final Eyer Project\App Interface\University-Road-Map-Project-mobile-app-interface"
+export "FLUTTER_APPLICATION_PATH=F:\Final Eyer Project\Map_Interface\University-Road-Map-Project-master\Interfaces"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build\ios"
