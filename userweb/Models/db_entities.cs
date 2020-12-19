aceb8c0466bb73b2d9db68d70c4d36db005dc9d2
Tharindu <tharindumadushanka.tm@gmail.com>
2020-12-19 22:45:34 +0530

    new admin panel

---
+++ userweb/Models/db_entities.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace userweb.Models
+{
+    public class db_entities : DbContext
+    {
+        public db_entities() : base("db_admin") { }
+        public DbSet<users> user { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<users>().ToTable("user");
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
